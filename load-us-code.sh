47152edf2bcf2b2940a639572e458e37fac17de6
Scott Stafford <sas4z@yahoo.com>
2013-09-12 21:46:45 -0400

    added DEMO_HOME env

--- load-us-code.sh
+++ load-us-code.sh
@@ -1,9 +1,9 @@
-mlcp.sh import -host localhost -port 8004 -username admin -password admin \
-               -input_file_path /home/scott/sandbox/us-code/data/xml/us/usc/ \
+DEMO_HOME=/home/scott/sandbox/ml7
+mlcp.sh import -host localhost -port 8041 -username admin -password admin \
+               -input_file_path $DEMO_HOME/data/xml/us/usc/ \
                -input_file_type documents \
                -document_type xml \
                -output_collections us-code \
-               -output_uri_replace "/home/scott/sandbox/us-code/data,'',.xml,''" \
+               -output_uri_replace "$DEMO_HOME/data,'',.xml,''" \
                -mode local \
                -streaming
-                
