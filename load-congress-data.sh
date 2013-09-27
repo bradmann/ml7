af915e5f8f47ed69717470b743e9296c477ab8c3
Scott Stafford <sas4z@yahoo.com>
2013-09-27 00:18:07 -0400

    remove load-congress-data.sh

--- load-congress-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir data/congress
-./s3curl.pl --id personal -- https://marklogic7-uscode-demo.s3.amazonaws.com/data/113.tar.gz -vv -o data/congress/113.tar.gz
-cd data/congress
-tar -xf 113.tar.gz
-rm 113.tar.gz
